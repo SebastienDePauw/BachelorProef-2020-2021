c16de9f0a7239933ee2a8a76f045922282a2e2b1
Sébastien De Pauw <depauws10@gmail.com>
2021-08-27 08:45:18 +0200

    onderzoek

--- android/bap_experiment_android/app/src/main/java/com/example/bap_experiment_android/ui/hoofdstuk4/activity/Hoofdstuk4Activity.kt
+++ android/bap_experiment_android/app/src/main/java/com/example/bap_experiment_android/ui/hoofdstuk4/activity/Hoofdstuk4Activity.kt
@@ -1,14 +1,27 @@
 package com.example.bap_experiment_android.ui.hoofdstuk4.activity
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_hoofdstuk4.*
 import com.example.bap_experiment_android.R
 import com.example.bap_experiment_android.ui.hoofdstuk4.viewmodel.Hoofdstuk4ViewModel
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_hoofdstuk4.button6
+import kotlinx.android.synthetic.main.activity_hoofdstuk4.button7_1
+import kotlinx.android.synthetic.main.activity_hoofdstuk4.button7_2
+import kotlinx.android.synthetic.main.activity_hoofdstuk4.button8
+import kotlinx.android.synthetic.main.activity_hoofdstuk4.button9
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
 
 
 class Hoofdstuk4Activity : AppCompatActivity() {
@@ -28,9 +41,88 @@ class Hoofdstuk4Activity : AppCompatActivity() {
 
         val viewModelFactory = Hoofdstuk4ViewModel.Factory()
         viewModel = ViewModelProvider(this, viewModelFactory).get(Hoofdstuk4ViewModel::class.java)
+        button6.setOnClickListener { startAsyncTask() }
+        button7_1.setOnClickListener { startThread() }
+        button7_2.setOnClickListener { startHandler() }
+        button8.setOnClickListener { startCoroutine() }
+        button9.setOnClickListener { startRxJava() }
     }
 
-    // region ViewModel observers
+    fun startAsyncTask() {
+        LongRunningOperation().execute();
+        for (i in 0..5) {
+            Thread.sleep(500)
+            Log.i("AsyncTask", "${i}")
+        }
+    }
 
-    // endregion
+    fun startCoroutine() = runBlocking {
+        LongRunningOperation2()
+        Log.i("Coroutine", "Done")
+    }
+
+    fun startHandler() {
+        val runnable = Runnable {
+            Thread.sleep(2000)
+            Log.i("Handler", "After two secs")
+        }
+
+        Handler().post(runnable)
+
+        for (i in 0..5) {
+            Thread.sleep(500)
+            Log.i("Handler", "${i}")
+        }
+        Log.i("Handler", "Done")
+
+
+    }
+    fun startRxJava() {
+        Observable
+            .interval(1, TimeUnit.SECONDS)
+            .doOnNext { n -> Log.i("RxJava", "${n}") }
+            .subscribe()
+        Log.i("RxJava","Done")
+    }
+
+    fun startThread() {
+        val runnable = Runnable {
+            Thread.sleep(2000)
+            Log.i("Thread", "After two secs")
+        }
+
+        Thread(runnable).start()
+
+        for (i in 0..5) {
+            Thread.sleep(500)
+            Log.i("Thread", "${i}")
+        }
+        Log.i("Thread", "Done")
+    }
+
+    class LongRunningOperation() : AsyncTask<Void, Void, String>() {
+        override fun doInBackground(vararg params: Void?): String? {
+            Thread.sleep(2000)
+            Log.i("AsyncTask", "After two secs")
+            return "Done"
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (result != null) {
+                Log.i("AsyncTask", result)
+            }
+        }
+    }
+
+    suspend fun LongRunningOperation2() = coroutineScope {
+        launch {
+            delay(2000)
+            Log.i("Coroutine", "After two secs!")
+        }
+        for (i in 0..5) {
+            delay(500)
+            Log.i("Coroutine", "${i}")
+        }
+    }
 }
--- android/bap_experiment_android/app/src/main/java/com/example/bap_experiment_android/ui/hoofdstuk5/activity/Hoofdstuk5Activity.kt
+++ android/bap_experiment_android/app/src/main/java/com/example/bap_experiment_android/ui/hoofdstuk5/activity/Hoofdstuk5Activity.kt
@@ -1,14 +1,27 @@
 package com.example.bap_experiment_android.ui.hoofdstuk5.activity
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.android.synthetic.main.activity_hoofdstuk5.*
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 import com.example.bap_experiment_android.R
 import com.example.bap_experiment_android.ui.hoofdstuk5.viewmodel.Hoofdstuk5ViewModel
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_hoofdstuk5.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import retrofit2.Retrofit
+import java.io.IOException
 
 
 class Hoofdstuk5Activity : AppCompatActivity() {
@@ -28,9 +41,76 @@ class Hoofdstuk5Activity : AppCompatActivity() {
 
         val viewModelFactory = Hoofdstuk5ViewModel.Factory()
         viewModel = ViewModelProvider(this, viewModelFactory).get(Hoofdstuk5ViewModel::class.java)
+
+        button10.setOnClickListener { startHttpUrl() }
+        button11.setOnClickListener { startOKHTTP() }
+        button12.setOnClickListener { startVolley() }
+        button13.setOnClickListener { startRetro() }
+        button14.setOnClickListener { startPic() }
+        button15.setOnClickListener { startGl() }
     }
 
-    // region ViewModel observers
+    fun startGl(){
+        val url = "https://source.unsplash.com/random?sig=0"
+        Glide.with(this)
+            .load(url)
+            .centerCrop()
+            .into(imageView)
+    }
 
-    // endregion
+    fun startHttpUrl(){
+        val url = URL("http://www.android.com/")
+        val urlConnection: HttpURLConnection
+                = url.openConnection() as HttpURLConnection
+        try {
+            val inp: InputStream
+                    = BufferedInputStream(urlConnection.getInputStream())
+        } finally {
+            urlConnection.disconnect()
+        }
+    }
+
+    fun startOKHTTP(){
+        val client = OkHttpClient()
+        val url = URL("http://www.android.com/")
+
+
+            val request = Request.Builder()
+                .url(url)
+                .build()
+
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful)
+                    throw IOException("Unexpected code $response")
+                //Do something with response
+            }
+
+    }
+
+    fun startPic(){
+        val imageUri = "https://source.unsplash.com/random?sig=0"
+        Picasso.get().load(imageUri).into(imageView)
+    }
+    fun startRetro(){
+
+    }
+    fun startVolley(){
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://www.android.com/"
+
+        val stringRequest = StringRequest(com.android.volley.Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                //Do something with response
+            },
+            Response.ErrorListener {
+                //Do something with error
+            })
+        queue.add(stringRequest)
+    }
+
+    data class User (val name: String)
+
 }
+
+
+
