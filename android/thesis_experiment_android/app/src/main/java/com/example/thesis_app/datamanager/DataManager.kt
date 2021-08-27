71aee49666214f73dd0e5b6f1b6a49e944f6be23
Sébastien De Pauw <depauws10@gmail.com>
2021-08-27 04:03:26 +0200

    onderzoek

--- android/thesis_experiment_android/app/src/main/java/com/example/thesis_app/datamanager/DataManager.kt
+++
@@ -1,5 +0,0 @@
-package com.example.thesis_app.datamanager
-
-object DataManager {
-
-}
--- android/thesis_experiment_android/app/src/main/java/com/example/thesis_app/ui/main/model/CardData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.thesis_app.ui.main.model
-
-data class CardData (
-    val cardEnum: CardEnum,
-    val titleRes: Int
-        )
